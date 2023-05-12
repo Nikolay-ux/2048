82430a9fb07c558626f60bbf4abd58aa23eb1fda
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-05-12 22:44:16 +0700

    Add fill random element in the table

---
+++ main.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <stdio.h>
+#include <cstdlib>
+#include <termios.h>
+#include <unistd.h>
+
+#define EDGE 4
+#define VAL 5
+
+using namespace std;
+
+void print(int A[][EDGE]) {
+    for(int i = 0; i < EDGE; i++) {
+        if(i == 0)
+            cout << "___________________________" << endl;
+        for(int j = 0; j < EDGE; j++) {
+            if(A[i][j] != 0)
+                printf("%5d |", A[i][j]);
+            else
+                printf("      |");
+        }
+        cout << endl;
+        if(i + 1 != EDGE)
+            cout << "------|------|------|------|" << endl;
+        else {
+            cout << "______|______|______|______|" << endl;
+        }
+    }
+}
+
+void zero(int A[][EDGE]) {
+    for(int i = 0; i < EDGE; i++) {
+        for(int j = 0; j < EDGE; j++) {
+            A[i][j] = 0;
+        }
+    }
+}
+
+int main()
+{
+
+    srand(time(NULL));
+    int area[EDGE][EDGE];
+    zero(area);
+    struct termios oldt, newt;
+    int ch = 1;
+    int element = rand() % (EDGE * EDGE);
+    int values[VAL] = {2, 2, 2, 2, 4};
+    area[element / EDGE][element % EDGE] = values[rand() % VAL];
+    system("clear");
+    print(area);
+    while (ch) {
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        switch (ch)
+        {
+        case 97:
+            // left();
+            //cout << "left" << endl;
+            break;
+        case 119:
+            // up();
+            //cout << "up" << endl;
+            break;
+        case 100:
+            // right();
+            //cout << "right" << endl;
+            break;
+        case 115:
+            // down();
+            //cout << "down" << endl;
+            break;
+        case 48:
+            return 0;
+            break;
+        default:
+            break;
+        }
+    }
+    return 0;
+}
