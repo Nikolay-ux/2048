82757d854bb18f3e6672d9bcdfc25f17fbb3ef3c
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-05-13 00:27:26 +0700

    Add function left and fill element in not empty table

--- main.cpp
+++ main.cpp
@@ -4,23 +4,25 @@
 #include <termios.h>
 #include <unistd.h>
 
-#define EDGE 4
-#define VAL 5
+const int edge = 4;
+const int val = 8;
 
 using namespace std;
 
-void print(int A[][EDGE]) {
-    for(int i = 0; i < EDGE; i++) {
+void print(int A[][edge]) {
+    for (int i = 0; i < edge; i++)
+    {
         if(i == 0)
             cout << "___________________________" << endl;
-        for(int j = 0; j < EDGE; j++) {
+        for (int j = 0; j < edge; j++)
+        {
             if(A[i][j] != 0)
                 printf("%5d |", A[i][j]);
             else
                 printf("      |");
         }
         cout << endl;
-        if(i + 1 != EDGE)
+        if (i + 1 != edge)
             cout << "------|------|------|------|" << endl;
         else {
             cout << "______|______|______|______|" << endl;
@@ -28,26 +30,110 @@ void print(int A[][EDGE]) {
     }
 }
 
-void zero(int A[][EDGE]) {
-    for(int i = 0; i < EDGE; i++) {
-        for(int j = 0; j < EDGE; j++) {
+void zero(int A[][edge])
+{
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++)
+        {
             A[i][j] = 0;
         }
     }
 }
 
+void add(int A[][edge])
+{
+    int values[val] = {2, 2, 2, 2, 2, 2, 2, 4};
+    int *index = (int*)malloc(sizeof(int));
+    int itr = 1;
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = 0; j < edge; j++)
+        {
+            if(A[i][j] == 0) {
+                index = (int*)realloc(index, itr*sizeof(int));
+                index[itr - 1] = i * edge + j;
+                itr++;
+            }
+        }
+    }
+    int randInd = index[rand() % (itr - 1)];
+    A[randInd / edge][randInd % edge] = values[rand() % val];
+}
+
+void left(int A[][edge])
+{
+    bool flag = false;
+    for (int i = 0; i < edge; i++)
+    {
+        for (int j = edge - 1; j > 0; j--)
+        {
+            if(A[i][j] != 0 && A[i][j - 1] == 0) {
+                A[i][j - 1] = A[i][j];
+                A[i][j] = 0;
+                flag = true;
+            }
+        }
+        for (int j = 0; j < edge; j++)
+        {
+            if (A[i][j] == A[i][j + 1])
+            {
+                A[i][j] += A[i][j + 1];
+                A[i][j + 1] = 0;
+                j++;
+            }
+        }
+    }
+    if(flag)
+        add(A);
+}
+
 int main()
 {
-
     srand(time(NULL));
-    int area[EDGE][EDGE];
+    int area[edge][edge];
     zero(area);
     struct termios oldt, newt;
     int ch = 1;
-    int element = rand() % (EDGE * EDGE);
-    int values[VAL] = {2, 2, 2, 2, 4};
-    area[element / EDGE][element % EDGE] = values[rand() % VAL];
+    int element = rand() % (edge * edge);
+    const int val = 8;
+    int values[val] = {2, 2, 2, 2, 2, 2, 2, 4};
+    area[element / edge][element % edge] = values[rand() % val];
     system("clear");
     print(area);
+    while (ch) {
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        switch (ch)
+        {
+        case 97:
+            left(area);
+            system("clear");
+            print(area);
+            //cout << "left" << endl;
+            break;
+        case 119:
+            // up();
+            //cout << "up" << endl;
+            break;
+        case 100:
+            // right();
+            //cout << "right" << endl;
+            break;
+        case 115:
+            // down();
+            //cout << "down" << endl;
+            break;
+        case 48:
+            return 0;
+            break;
+        default:
+            break;
+        }
+    }
     return 0;
 }
