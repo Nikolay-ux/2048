199aebb99c373e33e3db8ccb3a32bd7176ca7c5b
SokolovskiyNick <nikolael.pochta@gmail.com>
2023-05-21 01:42:27 +0700

    Add window opening

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <stdio.h>
 #include <cstdlib>
 #include <termios.h>
 #include <unistd.h>
 #include <string.h>
+
+#include <SFML/Graphics.hpp>
+
 #include "game.h"
 
 using namespace std;
@@ -14,40 +16,60 @@ int main()
     game game;
     game.add();
 
-    struct termios oldt, newt;
-    int ch = 1;
+    sf::RenderWindow window(sf::VideoMode(400, 500), "2048", sf::Style::Close | sf::Style::Titlebar);
+    window.setPosition(sf::Vector2i(760, 290));
 
-    while (ch)
+    sf::Vector2u windowSize(400, 500);
+    window.setSize(windowSize);
+
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Magenta);
+    while (window.isOpen())
     {
-        system("clear");
-        game.print();
-
-        tcgetattr(STDIN_FILENO, &oldt);
-        newt = oldt;
-        newt.c_lflag &= ~(ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-        ch = getchar();
-        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-        switch (ch)
+        sf::Event event;
+        while (window.pollEvent(event))
         {
-        case 97:
-            game.left();
-            break;
-        case 119:
-            game.up();
-            break;
-        case 100:
-            game.right();
-            break;
-        case 115:
-            game.down();
-            break;
-        case 48:
-            return 0;
-            break;
-        default:
-            break;
+            if (event.type == sf::Event::Closed)
+                window.close();
+            else if (event.type == sf::Event::Resized)
+                window.setSize(windowSize);
+            window.clear(sf::Color(200, 200, 200));
+            window.draw(shape);
+            window.display();
         }
     }
+    // struct termios oldt, newt;
+    // int ch = 1;
+    // while (ch)
+    // {
+    //     system("clear");
+    //     game.print();
+    //     tcgetattr(STDIN_FILENO, &oldt);
+    //     newt = oldt;
+    //     newt.c_lflag &= ~(ICANON | ECHO);
+    //     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    //     ch = getchar();
+    //     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    //     switch (ch)
+    //     {
+    //     case 97:
+    //         game.left();
+    //         break;
+    //     case 119:
+    //         game.up();
+    //         break;
+    //     case 100:
+    //         game.right();
+    //         break;
+    //     case 115:
+    //         game.down();
+    //         break;
+    //     case 48:
+    //         return 0;
+    //         break;
+    //     default:
+    //         break;
+    //     }
+    // }
     return 0;
 }
